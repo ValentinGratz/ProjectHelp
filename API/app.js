595a3703d4c68b47d4dcaed07bb80d67a8f87a95
SecretsMan <rayan.desfossez@outlook.fr>
2018-05-21 11:12:47 +0200

    Clean code

--- API/app.js
+++ API/app.js
@@ -13,16 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-const MONGO_ADDR = process.env.MONGO_ADDR;
-const MONGO_PORT = process.env.MONGO_PORT;
-const MONGO_DATABASE = process.env.MONGO_DATABASE;
-let db;
+const {MONGO_ADDR, MONGO_PORT, MONGO_DATABASE, PORT, JWT_SECRET} = process.env;
 
-const getConnectionString = () => {return `mongodb://${MONGO_ADDR}:${MONGO_PORT}`};
+const register = (db, username, mail, password, role) => {
 
-const register = (username, mail, password, role) => {
-
-	  return bcrypt.hash(password, 10).then((hash) => {
+	return bcrypt.hash(password, 10).then((hash) => {
 		const user = {
 			username,
 			mail,
@@ -37,50 +32,44 @@ const register = (username, mail, password, role) => {
 }
 
 
-const login = (username, password) => {
+const login = (db, username, password) => {
 	return db.collection("users").findOne({username}).then((result) => {
 		return bcrypt.compare(password, result.password).then((res) => {
 			if (res) {
 				const token = jwt.sign({
 					exp: 60,
 					data: {
-					id: result._id,
-					username: result.username,
-					mail: result.mail,
-					role: result.role
+						id: result._id,
+						username: result.username,
+						mail: result.mail,
+						role: result.role
 					}   
-			}, result.password);
+				}, JWT_SECRET);
 
-			return token;
-		} else {
-			return "";
-		}
+				return token;
 
+			} else return "";
 		});
-	}).catch((err) => {
-		return "";
-	});
-	
+	}).catch((err) => {return ""});
 }
 
 
-MongoClient.connect(getConnectionString()).then((client) => {
+MongoClient.connect(`mongodb://${MONGO_ADDR}:${MONGO_PORT}`).then((client) => {
+
 	db = client.db(MONGO_DATABASE);
-	console.log("Connection success");
+	console.log("CONNECTION SUCCESS");
+
+	app.post("/api/login", (req, res) => {
+
+		login(db, req.body.username, req.body.password).then((result) => {
+			res.json({token: result});
+		});
+	});
 });
 
 
-app.post("/api/login", (req, res) => {
-   login(req.body.username, req.body.password).then((result) => {
-	res.json({token: result});
-   });
-});
-
-
-const port = process.env.port || 3000;
-
-app.listen(port, () => {
-	console.log(`App listen on ${port}`);
+app.listen(PORT, () => {
+	console.log(`App listen on ${PORT}`);
 });
 
 
